b1558c52a3afe982740ca0f258edeca42a2c1c3d
BeNikk <bhattnik442@gmail.com>
2024-08-23 08:54:22 +0530

    Navbar

--- pdf/components/Navbar.tsx
+++ pdf/components/Navbar.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Star } from 'lucide-react';
 
 const Navbar = () => {
     return ( 
-        <div className="flex flex-row items-center justify-around h-16">
-            <div className="flex flex-row items-center gap-2">
-            <Image src={'/logoipsum-282.svg'} height={24} width={24} alt="logo"/>
-            <h1 className="font-medium font-mono text-md">PDF</h1>
+        <div className="flex flex-row h-16 items-center justify-around">
+            <div>
+                <Image src={'/logoipsum-281.svg'} alt="logo" width={24} height={24}/>
             </div>
             <div>
-
+                <a href="/">
+                <h1 className="font-medium text-sm">
+                    PDF
+                </h1>
+                </a>
+            </div>
+            <div>
+                <Link href={"https://github.com/BeNikk/pdf"}>
+                <Star className="w-5 h-5"/>
+                </Link>
             </div>
 
         </div>
